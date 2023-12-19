bbce73c899873910e106efaac2af9503043d6620
Israel Antonio Rosales Laguan <israellaguan@gmail.com>
2023-12-19 16:28:41 -0500

    feat: added legal content

--- app/legal/page.tsx
+++ app/legal/page.tsx
@@ -3,7 +3,54 @@ import { title } from "@/components/primitives";
 export default function LegalPage() {
 	return (
 		<div>
-			<h1 className={title()}>Legal</h1>
+			<h1 className={title()}>Legal and Terms</h1>
+
+			<h2>Terms and Conditions</h2>
+			<p>
+				<strong>Introduction</strong>
+			</p>
+			<p>
+				Welcome to Quiet-Lux! These terms and conditions outline the rules and regulations for the use of our website.
+			</p>
+			<p>
+				<strong>1. Acceptance of Terms</strong>
+			</p>
+			<p>
+				By accessing this website, you agree to be bound by these terms and conditions. If you disagree with any part
+				of these terms, please do not use our website.
+			</p>
+			<p>
+				<strong>2. Website Use</strong>
+			</p>
+			<p>
+				You may use our website for your own personal use and are not allowed to reproduce, duplicate, copy, sell,
+				resell, or exploit any portion of the service.
+			</p>
+			<p>
+				<strong>5. Legal Disclaimers</strong>
+			</p>
+			<p>We do not guarantee the accuracy, completeness, or timeliness of information on this website.</p>
+			<h2>Privacy Policy</h2>
+			<p>
+				<strong>Introduction</strong>
+			</p>
+			<p>Your privacy is important to us. This Privacy Policy outlines how your personal information is collected, used, and protected.</p>
+			<p>
+				<strong>1. Information Collection</strong>
+			</p>
+			<p>We collect personal information such as your name and email address when you use our website.</p>
+			<p>
+				<strong>2. Use of Information</strong>
+			</p>
+			<p>We use your information for authentication purposes. We do not sell, trade, or rent your personal information to third parties.</p>
+			<p>
+				<strong>3. Data Security</strong>
+			</p>
+			<p>We implement security measures to protect your personal information. However, no method of transmission over the internet or electronic storage is 100% secure.</p>
+			<p>
+				<strong>4. Changes to Privacy Policy</strong>
+			</p>
+			<p>We reserve the right to modify this Privacy Policy at any time. Changes and clarifications will be effective immediately.</p>
 		</div>
 	);
 }
